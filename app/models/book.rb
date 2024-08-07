d964ee5a63eacfb7ab26e29017566697750f2d8d
MariaHabib <maria.habib1102@gmail.com>
2024-08-07 19:53:37 +0500

    active record added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :author
+  has_one_attached :cover_image
   #validates :cover_image, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }, allow_blank: true
 end
